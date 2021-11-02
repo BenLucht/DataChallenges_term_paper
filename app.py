ef14af90394e192c8c38ed787f41d7cf042cff3b
BenLucht <benjamin.b.lucht@gmail.com>
2021-11-02 13:45:01 +0100

    basic scaffolding of project

---
+++ app.py
@@ -0,0 +1,13 @@
+# pylint: disable=E0611
+# pylint: disable=F0401
+# disables pylint errors for no name in module and unable to import
+
+import streamlit as st
+
+st.set_page_config(
+    page_title='Clustering Term Paper',
+    layout='wide',
+    initial_sidebar_state='expanded',
+)
+
+st.write('Data Challenges')
